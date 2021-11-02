ec43a6b92357c2e8f7ea3a52d35f956bcd126966
Bek <86363083+NotMeWinYouAlosMustFall@users.noreply.github.com>
2021-11-02 14:53:52 +0100

    Add files via upload
    changes to directory

--- server.py
+++ server.py
@@ -5,14 +5,26 @@ app = Flask(__name__)
 @app.route("/")
 def my_home():
     return render_template('index.html')
+@app.route("/index.html")
 
-@app.route("/<username>")
-def show_name(username=None):
-    return render_template('index.html', name= username)
-@app.route("/about")
+
+@app.route("/about.html")
 def about():
     return render_template('about.html')
 
-@app.route("/blog")
-def blog():
-    return "<p>These is a blog</p>"
+@app.route("/works.html")
+def works():
+    return render_template("works.html")
+
+@app.route("/work.html")
+def work():
+    return render_template("work.html")
+
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/components.html")
+def components():
+    return render_template("components.html")
